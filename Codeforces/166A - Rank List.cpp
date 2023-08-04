558c459eab1f58508463574bfd9c106226efd889
Phạm Hải Nam <113701208+runlnr@users.noreply.github.com>
2023-08-04 12:13:46 +0700

    Update 166A - Rank List.cpp
    C++

--- Codeforces/166A - Rank List.cpp
+++ Codeforces/166A - Rank List.cpp
@@ -9,10 +9,10 @@ using namespace std;
 typedef long long ll;   typedef string str;
 const ll MOD = 1e9+7, N = 1e5+7, J = 1e3+7;
 //chetmeorroiroiorr
-map<pair<int, int>, int> mp;
+map<pair<int, int>, int> cntTeams;
 bool vis[J][J];
-vector<int> sum;
-vector<pair<int, int>> vc;
+vector<int> pos;
+vector<pair<int, int>> teams;
 
 bool comp(pair<int, int> a, pair<int, int> b) {
     if(a.ff == b.ff) return a.ss < b.ss;
@@ -22,21 +22,21 @@ void solve(int n, int m) {
 
     for(int i = 0; i < n; i++) {
         int a, b;   cin >> a >> b;
-        mp[{a, b}]++;
-        if(!vis[a][b]) {vc.pback({a, b});   vis[a][b] = true;}
+        cntTeams[{a, b}]++;
+        if(!vis[a][b]) {teams.pback({a, b});   vis[a][b] = true;}
     }
-    sort(vc.begin(), vc.end(), comp);
+    sort(teams.begin(), teams.end(), comp);
 
-    for(int i = 0; i < vc.size(); i++) {
-        if(i == 0) sum.pback(mp[vc[i]]);
-        else sum.pback(mp[vc[i]] + sum[i-1]);
+    for(int i = 0; i < teams.size(); i++) {
+        if(i == 0) pos.pback(cntTeams[teams[i]]);
+        else pos.pback(cntTeams[teams[i]] + pos[i-1]);
     }
 
-    int upper = upper_bound(sum.begin(), sum.end(), m) - sum.begin();
-    if(sum[upper-1] == m) upper--;
+    int upper = upper_bound(pos.begin(), pos.end(), m) - pos.begin();
+    if(pos[upper-1] == m) upper--;
 
-    if(upper != 0) cout << sum[upper] - sum[upper-1];
-    else cout << sum[upper];
+    if(upper != 0) cout << pos[upper] - pos[upper-1];
+    else cout << pos[upper];
 }
 int main() {
     ios_base::sync_with_stdio(0);
