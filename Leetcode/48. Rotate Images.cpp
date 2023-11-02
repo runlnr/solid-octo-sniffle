ba11d2fe181c81063f401e86ad18793f1efe9548
Phạm Hải Nam <113701208+runlnr@users.noreply.github.com>
2023-11-02 21:30:21 +0700

    Update 48. Rotate Images.cpp

--- Leetcode/48. Rotate Images.cpp
+++ Leetcode/48. Rotate Images.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size()-1, perm = 0;
+        int n = matrix.size(), perm = 0;
+        n -= 1;
         int right, down, left, up;
 
         for(int i = perm; i < n-perm; i++) {
             for(int j = perm; j < n-perm; j++) {
-                right = matrix[i][j];
-                down = matrix[j][n-i];
-                left = matrix[n-i][n-j];
-                up = matrix[n-j][i];
+                right = matrix[i][j];        down = matrix[j][n-i];
+                left = matrix[n-i][n-j];     up = matrix[n-j][i];
 
                 matrix[j][n-i] = right;
                 matrix[n-i][n-j] = down;
