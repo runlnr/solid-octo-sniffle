b7f9200469b1ca640768240bd9abc1dbb31a841b
Phạm Hải Nam <113701208+runlnr@users.noreply.github.com>
2023-11-02 21:29:23 +0700

    Create 48. Rotate Images.cpp
    3ms / 7.41 MB

---
+++ Leetcode/48. Rotate Images.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size()-1, perm = 0;
+        int right, down, left, up;
+
+        for(int i = perm; i < n-perm; i++) {
+            for(int j = perm; j < n-perm; j++) {
+                right = matrix[i][j];
+                down = matrix[j][n-i];
+                left = matrix[n-i][n-j];
+                up = matrix[n-j][i];
+
+                matrix[j][n-i] = right;
+                matrix[n-i][n-j] = down;
+                matrix[n-j][i] = left;
+                matrix[i][j] = up;
+            }
+
+            perm++;
+        }
+        
+    }
+};
