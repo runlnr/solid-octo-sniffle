a0cc07c91a63b1ad0def6d007b6798cf76767a91
Phạm Hải Nam <113701208+runlnr@users.noreply.github.com>
2024-01-07 11:55:49 +0700

    Create 1919A - Wallet Exchange.cpp
    15ms / 0KB

---
+++ Codeforces/1919A - Wallet Exchange.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ss second
+#define ff first
+#define endl "\n"
+#define sp ' '
+#define pback push_back
+
+using namespace std;
+typedef long long ll;   typedef string str;
+const int MOD = 1e9+7, N = 1e5+7, J = 1e3+7;
+//chetmeorroiroiorr
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);     cout.tie(0);
+
+    int t;  cin >> t;
+    while(t--) {
+        int a, b;   cin >> a >> b;
+        ll ans = a+b;
+
+        if(ans%2 == 0) cout << "Bob";
+        else cout << "Alice";
+        cout << endl;
+    }
+    return 0;
+}
